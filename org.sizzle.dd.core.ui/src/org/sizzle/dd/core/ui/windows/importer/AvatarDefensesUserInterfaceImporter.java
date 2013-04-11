1097941b5199718dea0bb686ea338ee2d8ba43f9
jason-cech <jason.cech@live.com>
2013-04-11 01:38:45 -0600

    Starting work on the Defense section. The reasoning for the separation of the UI Importer and it's respective Top Component is that Lookup.getDefault().find(Class<T> c); seems to return a new instance. Therefore, if I have it seperated and it looks for its particular Top Component from WindowManager.getDefault().getOpenedTopComponents() and my instance is in the returned array, I know I should call the public configure method to update the avatar configuration.

---
+++ org.sizzle.dd.core.ui/src/org/sizzle/dd/core/ui/windows/importer/AvatarDefensesUserInterfaceImporter.java
@@ -0,0 +1,29 @@
+package org.sizzle.dd.core.ui.windows.importer;
+
+import org.openide.util.lookup.ServiceProvider;
+import org.openide.windows.Mode;
+import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
+import org.sizzle.dd.core.Avatar;
+import org.sizzle.dd.core.importer.AvatarUserInterfaceConfigurer;
+import org.sizzle.dd.core.ui.windows.DefensesTopComponent;
+
+/**
+ *
+ * @author Jason
+ */
+@ServiceProvider(service = AvatarUserInterfaceConfigurer.class)
+public class AvatarDefensesUserInterfaceImporter implements AvatarUserInterfaceConfigurer {
+
+    @Override
+    public void configure(Avatar t) {
+        Mode editorMode = WindowManager.getDefault().findMode("editor");
+        TopComponent[] findTopComponent = WindowManager.getDefault().getOpenedTopComponents(editorMode);
+        for (TopComponent topComponent : findTopComponent) {
+            if (topComponent.getClass().isAssignableFrom(DefensesTopComponent.class)) {
+                DefensesTopComponent.class.cast(topComponent).configure(t);
+            }
+        }
+    }
+    
+}
